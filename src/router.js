e43052a571d42fe5b539dcf76b7b236adba65016
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2023-11-15 12:06:44 +0000

    installed interleave, added skeleton of blog component

--- src/router.js
+++ src/router.js
@@ -10,6 +10,7 @@ import Resources from './Components/Resources/resources';
 import ResourceContainer from './Components/Resources/resourceContainer';
 import Y13HealthCheckFeedback from './Components/Resources/y13HealthCheckFeedback';
 import Privacy from './Components/Privacy/privacy';
+import Blog from './Components/blog/blog';
   
 //import ReactionsContainer from './components/reactionsContainer/reactionsContainer';
 //import Introduction from './components/introduction/Introduction';
@@ -23,6 +24,7 @@ import Privacy from './Components/Privacy/privacy';
       <Route path="/" element={<App />} >
         <Route path="/" element={<Landing />} />
         <Route path="/privacy/" element={<Privacy />} />
+        <Route path="/blog/" element={<Blog />} />
         <Route path="/resources/contents/" element={<Resources />} />             
           <Route path="/resources/:resource" element={<ResourceContainer />} /> 
           
