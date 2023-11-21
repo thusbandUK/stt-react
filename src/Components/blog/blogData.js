65e509a43d3b0f82dabf10460936d30f20967e35
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2023-11-21 10:15:46 +0000

    removed placeholder blog post

--- src/Components/blog/blogData.js
+++ src/Components/blog/blogData.js
@@ -1,5 +1,23 @@
-export const blogData = [
-    {
-        
-    }
-]
+/*holding
+
+THIS IS THE FORMAT FOR BLOG POSTS
+
+,
+        {
+            "blogTitle": "This is where your blog title would go",
+            "Date-created": "DATE CREATED",
+            "priority": "MANUALLY ENTERED NUMBER WHICH RANKS THE POSTS, IE IF IT'S 1, THAT POST COMES TOP OF CONTENTS",
+            "lastUpdated": "LAST UPDATED",
+            "meta-data": "META DATA",
+            "content": "Add all your html here",
+            "hyperLink": "THE SUFFIX OF THE URL UNIQUE TO THE POST",
+            "hyperLinkExternal": "BOOLEAN AS SCRIPT",
+            "blogDescription": "A blog post about personalising a chemistry lesson using references to a horror film for an adult student who loves the Saw movies"
+        }
+
+
+
+
+
+
+*/
