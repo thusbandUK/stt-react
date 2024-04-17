33dab750287d59628eb4c57eb7a281f2f8e26df8
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2024-04-17 05:28:01 +0100

    presentation file created in Flashcards folder in Components

---
+++ src/Components/flashcards/presentation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Presentation = () => {
+
+    return (
+        <div>
+            <p>Flashcards presentation page</p>
+        </div>
+    )
+}
+
+export default Presentation;
